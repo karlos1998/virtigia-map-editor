39ddce276570a7c653e9c3ca44327f26f1fd12fc
karlos1998 <fadeusz98@gmail.com>
2025-01-25 06:06:35 +0100

    fix shop relations with dialogs and npcs

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -49,7 +49,6 @@ class ShopController extends Controller
      */
     public function show(Shop $shop)
     {
-        dd($shop->dialogs);
         return Inertia::render('Shop/Show', [
             'shop' => $shop->only('id', 'name'),
             'items' => BaseItemResource::collection($shop->items),
