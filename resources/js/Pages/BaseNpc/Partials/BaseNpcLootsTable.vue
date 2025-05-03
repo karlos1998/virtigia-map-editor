ef3bf9c0778c85459d6e23d9b8f209b1dc7cd00e
karlos1998 <fadeusz98@gmail.com>
2025-05-03 17:12:09 +0200

    show item rarity next to loot item

--- resources/js/Pages/BaseNpc/Partials/BaseNpcLootsTable.vue
+++ resources/js/Pages/BaseNpc/Partials/BaseNpcLootsTable.vue
@@ -109,12 +109,11 @@ const detachItem = (item: BaseItemResource) => {
 <!--                                    <span class="font-medium text-surface-500 dark:text-surface-400 text-sm">{{ item.category }}</span>-->
                                     <div class="text-lg font-medium mt-2">{{ item.name }}</div>
                                 </div>
-<!--                                <div class="bg-surface-100 p-1" style="border-radius: 30px">-->
-<!--                                    <div class="bg-surface-0 flex items-center gap-2 justify-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">-->
-<!--&lt;!&ndash;                                        <span class="text-surface-900 font-medium text-sm">{{ item.rating }}</span>&ndash;&gt;-->
-<!--                                        <i class="pi pi-star-fill text-yellow-500"></i>-->
-<!--                                    </div>-->
-<!--                                </div>-->
+                                <div  v-if="item.rarity != 'common'" class="bg-surface-100 p-1" style="border-radius: 30px">
+                                    <div class="bg-surface-0 flex items-center gap-2 justify-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
+                                        <span  class="text-surface-900 font-medium text-sm">{{ item.rarity }}</span>
+                                    </div>
+                                </div>
                             </div>
                             <div class="flex flex-col md:items-end gap-8">
 <!--                                <span class="text-xl font-semibold">${{ item.price }}</span>-->
